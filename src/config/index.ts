d906e2daef1c4461d9995297fc6f635ee7816a6c
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2020-11-29 22:46:45 +0900

    feat: 사용자 회원가입시 비밀번호 암호화

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,15 +7,15 @@ if (foundDotenv.error) {
 }
 
 export default {
-  port: process.env.PORT,
+  port: process.env.PORT || "",
 
   db: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    db_name: process.env.DB_NAME,
+    host: process.env.DB_HOST || "",
+    port: process.env.DB_PORT || "",
+    user: process.env.DB_USER || "",
+    pass: process.env.DB_PASS || "",
+    db_name: process.env.DB_NAME || "",
   },
 
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret: process.env.JWT_SECRET || "",
 };
--- src/service/user/register.ts
+++ src/service/user/register.ts
@@ -1,10 +1,16 @@
 import { IUserRegister } from "../../interfaces/User";
 import User from "../../database/models/user.models";
 import logger from "../../loaders/logger";
+import * as bcrypt from "bcrypt";
 
 const UserRegister = async (user: IUserRegister) => {
   try {
-    const createdUser = await User.create(user);
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const createdUser = await User.create({
+      ...user,
+      password: hashedPassword,
+    });
 
     if (!createdUser) {
       throw new Error("Register Error");
